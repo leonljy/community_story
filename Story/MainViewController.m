a4ed25f928a2383623276805314995a3c1356cdb
John Kim <joimkim@gmail.com>
2015-10-06 20:47:31 -0700

    Added FeaturedTableViewCell for the MainTableView. Fetched data from Parse. NSPredicate checks Story PFObject for isEnded then orders by, writerCount, currentSequence, and finally createdTime.

--- Story/MainViewController.m
+++ Story/MainViewController.m
@@ -8,17 +8,52 @@
 
 #import "MainViewController.h"
 #import "NewStoryViewController.h"
+#import <Parse/Parse.h>
+#import "FeaturedTableViewCell.h"
 
 
 @interface MainViewController ()
+
+@property (strong, nonatomic) NSArray *storyArray;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation MainViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+  /* Using NSPredicate. Checks if story isEnded and the sorts by writersCount. In case of tie further sort by sentence count and in case of tie futher sort by created time. */
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"isEndedStory = NO"];
+    PFQuery *query = [PFQuery queryWithClassName:@"Story" predicate:predicate];
+    [query orderByDescending:@"writersCount"];
+    [query addDescendingOrder:@"currentSequence"];
+    [query addAscendingOrder:@"createdAt"];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            //            NSLog(@"Successfully retrieved %lu scores.", (unsigned long)objects.count);
+            self.storyArray = objects;
+            for(PFObject *story in self.storyArray){
+                
+             /*   NSLog(@"WC: %ld, CS: %ld, Created: %@", [story[@"writersCount"] integerValue], [story[@"currentSequence"] integerValue], story.createdAt); */
+            }
+            [self.tableView reloadData];
+            // Do something with the found objects
+            for (PFObject *object in objects) {
+                NSLog(@"%@", object.objectId);
+            }
+        } else {
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,14 +67,58 @@
     [self.navigationController pushViewController:newStoryViewController animated:YES];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//tableView
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.storyArray count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *FeaturedCellIdentifier = @"FEATURED_STORY_CELL";
+//    static NSString *StandardCellIdentifier = @"STANDARD_STORY_CELL";
+ 
+    //TODO need to change resuable cell identifier if section is greater than 1
+    PFObject *story = [self.storyArray objectAtIndex:indexPath.row];
+    FeaturedTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:FeaturedCellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[FeaturedTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:FeaturedCellIdentifier];
+    }
+    
+    
+    cell.featuredCellTitle.text = story[@"title"];
+    cell.featuredCellDescription.text = story[@"description"];
+    cell.featuredStoryText.text = story[@"text"];
+    cell.featuredName.text = story[@"ownerName"];
+    
+//    
+//    PFUser *owner = story[@"owner"];
+//    PFQuery *query = [PFUser query];
+//    [query whereKey:@"objectId" equalTo:owner.objectId];
+//    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+//        if(error){
+//            
+//        }else{
+//            if(0<[objects count]){
+//                PFUser *user = [objects firstObject];
+//                cell.featuredName.text = user.username;
+//            }else{
+//                
+//            }
+//        }
+//    }];
+
+    return cell;
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return self.tableView.frame.size.height - 20;
+}
 
 @end
---
+++ Story/StandardTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  StandardTableViewCell.h
+//  Story
+//
+//  Created by john kim on 10/6/15.
+//  Copyright © 2015 Favorie&John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StandardTableViewCell : UITableViewCell
+
+@end
