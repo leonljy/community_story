58dcd78835660724bb79f55e7d15676c36cbaa46
Kwanghwi Kim <kksra@naver.com>
2015-10-15 18:06:02 +0900

    Assign story data to DetailStoryTextViewController.

---
+++ Story/DetailFirstSentenceTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DetailFirstSentenceTableViewCell.m
+//  Story
+//
+//  Created by Kwanghwi Kim on 2015. 10. 15..
+//  Copyright © 2015년 Favorie&John. All rights reserved.
+//
+
+#import "DetailFirstSentenceTableViewCell.h"
+
+@implementation DetailFirstSentenceTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
