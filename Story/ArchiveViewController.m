a897b2e9dee140c218f8da48341b4d66751d5e66
Jeonguk Lee <julee@favorie.co>
2015-10-19 19:23:31 +0900

    Adjust UI Constraints in MainVC and AchiveVC

--- Story/ArchiveViewController.m
+++ Story/ArchiveViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ArchiveViewController.h"
+#import "FeaturedStoryCell.h"
+#import "PFObject+Story.h"
 
-@interface ArchiveViewController ()
-
+@interface ArchiveViewController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (strong, nonatomic) NSArray *stories;
 @end
 
 @implementation ArchiveViewController
@@ -17,7 +20,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.collectionView setDelegate:self];
+    [self.collectionView setDataSource:self];
+    
+    [PFObject storiesArchievedWithSuccessBlock:^(NSArray *objects) {
+        self.stories = objects;
+        [self.collectionView reloadData];
+    } failureBlock:^(NSError *error) {
+        NSLog(@"Error: %@ %@", error, [error userInfo]);
+    }];
+
 }
+
+#pragma mark - UICollectoinView Delegates
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section{
+    return 10.0f;
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CGFloat heightCell = 250;
+    return CGSizeMake(self.collectionView.frame.size.width, heightCell);
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return [self.stories count];
+}
+-(void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    FeaturedStoryCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CELL_ACHIEVED_STORY" forIndexPath:indexPath];
+    PFObject *story = [self.stories objectAtIndex:indexPath.row];
+    [cell setFeaturedStoryDatasToUI:story];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
